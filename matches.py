32736b9cb5f22485e0667c8d103fa19f13b2e01e
Rob Spanton <rob@robspanton.com>
2014-03-13 22:27:29 +0000

    Add support for multiple delays
    This adds support for a list of delays, and adjusts match times
    accordingly.  So all match times will continue to be correct.
    
    If a delay is set to occur *during* a match, then only the matches
    that follow it will be affected.

--- matches.py
+++ matches.py
@@ -1,5 +1,6 @@
 "Match schedule library"
 from collections import namedtuple
+from datetime import timedelta
 import datetime
 import time
 import yaml
@@ -15,6 +16,9 @@ MatchPeriod = namedtuple("MatchPeriod",
 Match = namedtuple("Match",
                    ["num", "arena", "teams", "start_time", "end_time"])
 
+Delay = namedtuple("Delay",
+                   ["delay", "time"])
+
 class MatchSchedule(object):
     def __init__(self, config_fname):
         with open(config_fname, "r") as f:
@@ -26,9 +30,22 @@ class MatchSchedule(object):
                                                  e["end_time"]))
 
         self.match_period = datetime.timedelta(0, y["match_period_length_seconds"])
-        self.current_delay = datetime.timedelta(0, y["current_delay"])
 
+        self._build_delaylist(y["delays"])
         self._build_matchlist(y["matches"])
+
+    def _build_delaylist(self, yamldata):
+        delays = []
+        for info in yamldata:
+            d = Delay(timedelta(seconds = info["delay"]),
+                      info["time"])
+            delays.append(d)
+
+        # Ensure the delays are sorted by time
+        def cmpdelay(x,y):
+            return cmp(x.time, y.time)
+
+        self.delays = sorted(delays, cmp = cmpdelay)
 
     def _build_matchlist(self, yamldata):
         "Build the match list"
@@ -36,11 +53,20 @@ class MatchSchedule(object):
         match_numbers = sorted(yamldata.keys())
 
         # First match starts at this point
-        current_start = self.match_periods[0].start_time + self.current_delay
+        current_start = self.match_periods[0].start_time
+
+        # The current delay
+        current_delay = datetime.timedelta()
+
+        # We'll pop items off this list as we go
+        delays = list(self.delays)
 
         for period in self.match_periods:
             # Fill this period with matches
-            start = period.start_time + self.current_delay
+            start = period.start_time
+
+            while len(delays) and delays[0].time <= start:
+                start += delays.pop(0).delay
 
             # Fill this match period with matches
             while start < period.end_time:
@@ -57,6 +83,8 @@ class MatchSchedule(object):
                     self.matches[matchnum][arena_name] = match
 
                 start += self.match_period
+                while len(delays) and delays[0].time <= start:
+                    start += delays.pop(0).delay
 
     def n_matches(self):
         return len(self.matches)
