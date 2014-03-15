62fe0915ad624030345a03b8e9737408d94bd0ec
Rob Spanton <rob@robspanton.com>
2014-03-15 14:41:18 +0000

    Support the new match period YAML format
    The match period data now contains the time of the knockout.  This
    adds support for the new format.

--- matches.py
+++ matches.py
@@ -25,7 +25,7 @@ class MatchSchedule(object):
             y = yaml.load(f.read(), Loader = YAML_Loader)
 
         self.match_periods = []
-        for e in y["match_sets"]:
+        for e in y["match_periods"]["league"]:
             if "max_end_time" in e:
                 max_end_time = e["max_end_time"]
             else:
