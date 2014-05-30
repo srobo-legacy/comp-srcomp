6c3c214f9beb36707df4723282f069e9ae61f493
Peter Law <PeterJCLaw@gmail.com>
2014-05-30 22:25:01 +0100

    Change to loading the scorer dynamically.
    While for the moment the values are all literals, this provides a
    possible mechanism for the scorer class to be both located and then
    loaded in a dynamic manner. This would make it possible to have the
    scorer implementation specified as a config option, or even as part
    of the competition state.
    
    Change-Id: Iadb6e36ae5d641553bf7757699142737454f2e08

--- comp.py
+++ comp.py
@@ -1,5 +1,6 @@
 
 from collections import namedtuple
+import imp
 import os
 from subprocess import check_output
 
@@ -8,14 +9,24 @@ import matches
 import scores
 import teams
 
-from scorer import Scorer
+_scorer = None
+
+def _get_scorer():
+    global _scorer
+    if _scorer is None:
+        mydir = os.path.dirname(os.path.abspath(__file__))
+        root = os.path.join(mydir, "scoring-2014/lib")
+        imp.load_source("score_logic", os.path.join(root, "score_logic.py"))
+        m = imp.load_source("scorer", os.path.join(root, "scorer.py"))
+        _scorer = getattr(m, "Scorer")
+    return _scorer
 
 class SRComp(object):
     def __init__(self, root):
         self.root = root
         self.state = check_output("git rev-parse HEAD", shell=True, cwd=root).strip()
         self.teams = teams.load_teams(os.path.join(root, "teams.yaml"))
-        self.scores = scores.Scores(root, self.teams.keys(), Scorer)
+        self.scores = scores.Scores(root, self.teams.keys(), _get_scorer())
         self.arenas = arenas.load_arenas(os.path.join(root, "arenas.yaml"))
         schedule_fname = os.path.join(root, "schedule.yaml")
         self.schedule = matches.MatchSchedule.create(schedule_fname,
