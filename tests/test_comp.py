eceb3dc1788685e3d5f8f6230baecb985fc07e61
Alastair Lynn <arplynn@gmail.com>
2015-02-10 20:14:00 +0000

    Import timezone information from the compstate repo

--- tests/test_comp.py
+++ tests/test_comp.py
@@ -1,20 +1,26 @@
 
 import os
+import datetime
 
 # Hack the path
 import helpers as test_helpers
 
 from sr.comp.comp import SRComp
 
+DUMMY_PATH = os.path.dirname(os.path.abspath(__file__)) + '/dummy'
+INSTANCE = SRComp(DUMMY_PATH)
+
 def test_load():
     "Test that loading the dummy state works"
-    my_dir = os.path.dirname(os.path.abspath(__file__))
-    instance = SRComp(my_dir + '/dummy')
+    assert INSTANCE.root
+    assert INSTANCE.state
+    assert INSTANCE.teams
+    assert INSTANCE.schedule
+    assert INSTANCE.scores
+    assert INSTANCE.arenas
+    assert INSTANCE.corners
 
-    assert instance.root
-    assert instance.state
-    assert instance.teams
-    assert instance.schedule
-    assert instance.scores
-    assert instance.arenas
-    assert instance.corners
+def test_timezone():
+    # Test that one can get the timezone from the dummy state
+    assert (INSTANCE.timezone.utcoffset(datetime.datetime(2014, 4, 26)) ==
+            datetime.timedelta(seconds=3600))
