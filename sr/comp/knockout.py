0e9acf0c98abd2d5b5507e8bf747830092a666d2
Alistair Lynn <arplynn@gmail.com>
2015-02-10 14:06:02 +0000

    PEP8 cleanup on `knockout.py`
    No functional change.

--- sr/comp/knockout.py
+++ sr/comp/knockout.py
@@ -1,13 +1,16 @@
 import math
 
+
 def bit_mask(n):
     "Return an n-bit mask of 1's"
-    return 2 ** n -1
+    return 2 ** n-1
+
 
 def reverse_bits(n, width):
     "Reverse the bits of n"
     b = '{:0{width}b}'.format(n, width=width)
     return int(b[::-1], 2)
+
 
 def first_round_seeding(n_teams):
     """Return the seed arrangement for the first round of a knockout with n_teams
@@ -15,13 +18,13 @@ def first_round_seeding(n_teams):
     Returns an array of matches."""
 
     # Round the number of teams up to a power of two
-    rounded_teams = int( 2 ** math.ceil( math.log( n_teams, 2 ) ) )
+    rounded_teams = int(2 ** math.ceil(math.log(n_teams, 2)))
 
-    n_bits = int( math.ceil( math.log( rounded_teams, 2 ) ) )
+    n_bits = int(math.ceil(math.log(rounded_teams, 2)))
 
     n_per_match = 4
     n_matches = int(math.ceil(float(rounded_teams) / n_per_match))
-    matches_bits = int( math.ceil( math.log( n_matches, 2 ) ) )
+    matches_bits = int(math.ceil(math.log(n_matches, 2)))
 
     # Find the order in which we repeatedly insert teams into the
     # match list.
@@ -34,11 +37,11 @@ def first_round_seeding(n_teams):
     ins_order = []
     for n in range(n_matches):
         if n % 2 == 0:
-            "Even"
-            v = reverse_bits( n, matches_bits )
+            # Even
+            v = reverse_bits(n, matches_bits)
         else:
-            "Odd"
-            v ^= bit_mask( matches_bits )
+            # Odd
+            v ^= bit_mask(matches_bits)
         ins_order.append(v)
 
     matches = []
@@ -46,6 +49,6 @@ def first_round_seeding(n_teams):
         matches += [[]]
 
     for n in range(n_teams):
-        matches[ ins_order[ n % n_matches ] ].append( n )
+        matches[ins_order[n % n_matches]].append(n)
 
     return matches
