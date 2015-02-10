006d5e3eec8f29e66782c6a4157392e46e272337
Alistair Lynn <arplynn@gmail.com>
2015-02-10 14:00:21 +0000

    PEP8 cleanup on the YAML loader
    No functional change.

--- sr/comp/yaml_loader.py
+++ sr/comp/yaml_loader.py
@@ -13,12 +13,15 @@ except ImportError:
           "files, this is liable to be very slow. Installing libyaml "
           "is highly recommended.", file=sys.stderr)
 
+
 def time_constructor(loader, node):
     return dateutil.parser.parse(node.value)
 
-YAML_Loader.add_constructor( "tag:yaml.org,2002:timestamp",
-                             time_constructor )
+
+YAML_Loader.add_constructor("tag:yaml.org,2002:timestamp",
+                            time_constructor)
+
 
 def load(file_path):
     with open(file_path, 'r') as f:
-        return yaml.load(f, Loader = YAML_Loader)
+        return yaml.load(f, Loader=YAML_Loader)
