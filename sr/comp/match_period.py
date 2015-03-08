8deaec78f79bd98b167f0ac407ae177d790c7b76
Tom Leese <inbox@tomleese.me.uk>
2015-03-08 15:40:27 +0000

    Pass list of attributes to namedtuple

--- sr/comp/match_period.py
+++ sr/comp/match_period.py
@@ -1,10 +1,13 @@
 from collections import namedtuple
 from enum import Enum, unique
 
-MatchPeriod = namedtuple('MatchPeriod', 'start_time end_time max_end_time '
-                                        'description matches')
-Match = namedtuple('Match', 'num arena teams '
-                            'start_time end_time type')
+
+MatchPeriod = namedtuple('MatchPeriod', ['start_time', 'end_time',
+                                         'max_end_time', 'description',
+                                         'matches'])
+Match = namedtuple('Match', ['num', 'arena', 'teams', 'start_time', 'end_time',
+                             'type'])
+
 
 @unique
 class MatchType(Enum):
