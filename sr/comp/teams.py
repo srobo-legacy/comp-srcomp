ce33f64c2cea6343921a06ddd54554b9238879a7
Tom Leese <inbox@tomleese.me.uk>
2015-03-11 08:51:55 +0000

    Minor formatting improvements

--- sr/comp/teams.py
+++ sr/comp/teams.py
@@ -1,22 +1,24 @@
-"Team metadata library"
+"""Team metadata library."""
 from collections import namedtuple
 
 from . import yaml_loader
 
-Team = namedtuple("Team",
-                  ["tla", "name", "rookie"])
+
+Team = namedtuple('Team', ['tla', 'name', 'rookie'])
 
 
-def load_teams(fname):
-    "Load teams from a YAML file"
+def load_teams(filename):
+    """
+    Load teams from a YAML file.
 
-    y = yaml_loader.load(fname)
+    :param str filename: The filename of the YAML file to load."""
+    data = yaml_loader.load(filename)
 
     teams = {}
-    for tla, info in y["teams"].items():
+    for tla, info in data['teams'].items():
         tla = tla.upper()
         teams[tla] = Team(tla=tla,
-                          name=info["name"],
-                          rookie=info.get("rookie", False))
+                          name=info['name'],
+                          rookie=info.get('rookie', False))
 
     return teams
