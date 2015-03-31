044f9009276575b6d490a298db2572e29438ca05
Tom Leese <inbox@tomleese.me.uk>
2015-03-31 10:56:34 +0100

    Improve formatting of setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import find_packages, setup
 
 
 with open('README.rst') as f:
-    description = f.read()
+    long_description = f.read()
 
 install_requires = [
     'PyYAML >=3.11, <4',
@@ -16,16 +16,19 @@ install_requires = [
 if sys.version_info < (3, 4):
     install_requires.append('enum34 >=1.0.4, <2')
 
-setup(name='sr.comp',
-      version='1.0.0',
-      packages=find_packages(),
-      namespace_packages=['sr', 'sr.comp'],
-      description=description,
-      author='Student Robotics Competition Software SIG',
-      author_email='srobo-devel@googlegroups.com',
-      install_requires=install_requires,
-      setup_requires=[
-          'nose >=1.3, <2',
-          'Sphinx >=1.2, <2'
-      ],
-      test_suite='nose.collector')
+setup(
+    name='sr.comp',
+    version='1.0.0',
+    packages=find_packages(),
+    namespace_packages=['sr', 'sr.comp'],
+    description='Student Robotics Competition Software',
+    long_description=long_description,
+    author='Student Robotics Competition Software SIG',
+    author_email='srobo-devel@googlegroups.com',
+    install_requires=install_requires,
+    setup_requires=[
+        'nose >=1.3, <2',
+        'Sphinx >=1.2, <2'
+    ],
+    test_suite='nose.collector'
+)
