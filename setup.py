17d948aabd7ceaa323e8b5816469efe6b4bce96a
Peter Law <PeterJCLaw@gmail.com>
2015-04-14 23:20:58 +0100

    Bump Sphinx requirement to 1.3
    The Alabaster theme was added in 1.3
    
    Change-Id: Ie9336c618e5b94f05085fee00268486f15f669f5

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ setup(
     install_requires=install_requires,
     setup_requires=[
         'nose >=1.3, <2',
-        'Sphinx >=1.2, <2'
+        'Sphinx >=1.3, <2'
     ],
     test_suite='nose.collector'
 )
