fe341fa2be9065a364d917a836c7eeeb13fc8af1
Peter Law <PeterJCLaw@gmail.com>
2015-04-14 21:36:14 +0100

    Remove leading 0s which Python 3 doesn't like
    Change-Id: I1d59be52d1d4d88bf45b3183112e5d78c4687a99

--- tests/test_match_period.py
+++ tests/test_match_period.py
@@ -4,8 +4,8 @@ from datetime import datetime
 from sr.comp.match_period import MatchPeriod
 
 def test_period_str():
-    start = datetime(2014, 01, 01, 13, 12, 14)
-    end = datetime(2014, 01, 01, 20, 06, 35)
+    start = datetime(2014, 1, 1, 13, 12, 14)
+    end = datetime(2014, 1, 1, 20,  6, 35)
     period = MatchPeriod(start, end, None, "desc", None, None)
 
     string = str(period)
